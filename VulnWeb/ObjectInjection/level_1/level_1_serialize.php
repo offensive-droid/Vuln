08f98043400724df23a0a868a68d24cc69b0de4e
offensive-droid <138177031+offensive-droid@users.noreply.github.com>
2024-01-01 07:43:47 +0800

    Delete VulnWeb directory

--- VulnWeb/ObjectInjection/level_1/level_1_serialize.php
+++
@@ -1,9 +0,0 @@
-<?php
-class PHPObjectInjection{
-    public $inject = "system('whoami');";
-}
-$obj = new PHPObjectInjection;
-$serialized = serialize($obj);
-echo $serialized;
-
-?>
--- VulnWeb/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$mysqli = new mysqli("localhost","root","","newyear");
-if($mysqli -> connect_errno){
-    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-    exit();
-}?>
-
