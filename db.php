7fabffb2d15d012aae5eab3157de184d9ca4a268
offensive-droid <138177031+offensive-droid@users.noreply.github.com>
2024-01-01 15:33:42 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$mysqli = new mysqli("localhost","root","","newyear");
+$mysqli = new mysqli("mysql_db","root","root","vulnapp");
 if($mysqli -> connect_errno){
     echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
     exit();
