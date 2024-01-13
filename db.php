4e20fffab03d6acf5282fcf859ff71d7d9ca68c2
offensive-droid <138177031+offensive-droid@users.noreply.github.com>
2024-01-13 23:46:51 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,9 @@
 <?php
 session_start();
-$mysqli = new \MySQLi("mysql_db","root","root","vulnapp");
+$mysqli = new MySQLi("mysql_db","root","root","vulnapp");
 if($mysqli -> connect_errno){
     echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
     exit();
+    echo 'idk';
 }?>
 
